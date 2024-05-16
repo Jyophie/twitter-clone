f7e33d23446fb9c1ffae866f045a4abf9e7aff23
Jyophie <jyophie@gmail.com>
2024-05-16 17:35:37 +0900

    Feat: tweet db at firestore

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,124 @@
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { auth, db } from "../firebase";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitButton = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+export default function PostTweetForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length > 0) {
+      setFile(files[0]);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) {
+      return;
+    }
+    try {
+      setIsLoading(true);
+      await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        user: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <TextArea
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        value={tweet}
+        placeholder="What is happening?"
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "Photo Added ✅" : "Add photo"}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        id="file"
+        type="file"
+        accept="image/*"
+      />
+      <SubmitButton
+        type="submit"
+        value={isLoading ? "Posting..." : "Post Tweet"}
+      />
+    </Form>
+  );
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,19 +1,14 @@
-import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import PostTweetForm from "../components/post-tweet-form";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
 
 export default function Home() {
-  const navigate = useNavigate();
-
-  const logOut = () => {
-    auth.signOut();
-    navigate("/login");
-  };
-
   return (
-    <>
-      <h1>
-        <button onClick={logOut}>Log Out</button>
-      </h1>
-    </>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 }
